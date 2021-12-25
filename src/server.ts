f633d96d0a01e4af6cf190961f3fd2218f46e1a6
neresleonardo <leonardo.borges.neres@gmail.com>
2021-12-25 19:28:55 -0300

    database

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,9 @@
+import "reflect-metadata";
 import express from "express";
+import "./database"
+
 const app = express();
 
-// GET - BUSCAR POR INFORMAÇÃO 
-// POST - INSERIR INFROMAÇÃO
-// PUT - ALTERAR INFORMAÇÃO 
-// DELETAR - DEMOVER DADOS
-// PATCH - ALTERAR IFORMAÇÃO ESPECIFICA 
 
-app.get("/test", (req,res) => {
-    return res.send("Texte de rota") 
-})
-
-app.post("/test-post", (req,res) => {
-    return res.send("texte de post")
-})
 
 app.listen(3000, () => console.log("Server is runner port 3000"));

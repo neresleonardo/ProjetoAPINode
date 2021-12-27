0af694e34b5cf57e5219f7d0d749fd5eb40bb1a8
Leonardo Borges <leonardo.borges.neres@gmail.com>
2021-12-27 11:42:29 -0300

    comentario

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import "reflect-metadata";
-import { router } from "./Router"
-import express from "express";
-import "./database"
+import "reflect-metadata"; // Importado por causa do TypeScript
+import { router } from "./Router" // Rotas da Api
+import express from "express"; // Biblioteca Express
+import "./database" // Banco de Dados
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // Formato que vai ser entrege
 
-app.use(router);
+app.use(router); // Rotas 
 
-app.listen(3000, () => console.log("Server is runner port 3000"));
+app.listen(3000, () => console.log("Server is runner port 3000")); // Porta do server 

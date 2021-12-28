cf77ffaafb3a476ada285b47b25aa7c674160485
Leonardo Borges <leonardo.borges.neres@gmail.com>
2021-12-27 21:10:53 -0300

    Middleware

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,24 @@
 import "reflect-metadata"; // Importado por causa do TypeScript
 import { router } from "./Router" // Rotas da Api
-import express from "express"; // Biblioteca Express
+import express, { Request, Response, NextFunction } from "express"; // Biblioteca Express
 import "./database" // Banco de Dados
 const app = express();
 
 app.use(express.json()); // Formato que vai ser entrege
 
 app.use(router); // Rotas 
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof Error) {
+        return response.status(400).json({
+            error: err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error "
+    })
+})
 
 app.listen(3000, () => console.log("Server is runner port 3000")); // Porta do server 

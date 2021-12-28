88a57535e46e9a3fb04bf04c58d9fd3705a0d240
Leonardo Borges <leonardo.borges.neres@gmail.com>
2021-12-28 19:25:28 -0300

    middlewares

--- src/Router.ts
+++ src/Router.ts
@@ -1,6 +1,7 @@
 import {  Router } from "express" // Importando Rotas do empress
 import { CreateUserController } from "./controllers/CreateUserController" // Importando o controle do usuario 
 import { CreateTagController } from "./controllers/CreateTagController" // Importando o controle do usuario 
+import { ensureAdmin } from "./middlewares/ensureAdmin"
 
 const router = Router() // Rota
 
@@ -9,6 +10,6 @@ const createTagController = new CreateTagController();
 
 router.post("/users", createUserController.handle) // Criando uma rota post para criar usuarios 
 
-router.post("/tags", createTagController.handle) // Criando uma rota post para criar uma tag 
+router.post("/tags", ensureAdmin, createTagController.handle) // Criando uma rota post para criar uma tag 
 
 export { router } // Exportando rota

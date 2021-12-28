66a3578073d04ce9d6b9a045f141cf2ff24c60d6
Leonardo Borges <leonardo.borges.neres@gmail.com>
2021-12-28 18:39:19 -0300

    Create Tags

--- src/Router.ts
+++ src/Router.ts
@@ -1,10 +1,14 @@
 import {  Router } from "express" // Importando Rotas do empress
 import { CreateUserController } from "./controllers/CreateUserController" // Importando o controle do usuario 
+import { CreateTagController } from "./controllers/CreateTagController" // Importando o controle do usuario 
 
 const router = Router() // Rota
 
 const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
 
 router.post("/users", createUserController.handle) // Criando uma rota post para criar usuarios 
+
+router.post("/tags", createTagController.handle) // Criando uma rota post para criar uma tag 
 
 export { router } // Exportando rota

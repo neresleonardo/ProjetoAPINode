83e979ed68f4ca22ded550a0bbd0146cf8885955
Leonardo Borges <leonardo.borges.neres@gmail.com>
2021-12-29 20:57:09 -0300

    Create Compliment Controller

--- src/Router.ts
+++ src/Router.ts
@@ -3,15 +3,18 @@ import { CreateUserController } from "./controllers/CreateUserController" // Imp
 import { CreateTagController } from "./controllers/CreateTagController" // Importando o controle do usuario 
 import { ensureAdmin } from "./middlewares/ensureAdmin"
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController"
+import { CreateComplimentController } from "./controllers/CreateComplimentController"
 
 const router = Router() // Rota
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController()
+const createComplimentController = new CreateComplimentController()
 
 router.post("/users", createUserController.handle) // Criando uma rota post para criar usuarios 
 router.post("/login", authenticateUserController.handle)
 router.post("/tags", ensureAdmin, createTagController.handle) // Criando uma rota post para criar uma tag 
+router.post("/compliment", createComplimentController.handle )
 
 export { router } // Exportando rota

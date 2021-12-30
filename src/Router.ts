6f98bfb308c94e3098f3368b36499a44863cd2a8
Leonardo Borges <leonardo.borges.neres@gmail.com>
2021-12-29 21:56:44 -0300

    list Tag

--- src/Router.ts
+++ src/Router.ts
@@ -1,20 +1,58 @@
-import {  Router } from "express" // Importando Rotas do empress
-import { CreateUserController } from "./controllers/CreateUserController" // Importando o controle do usuario 
-import { CreateTagController } from "./controllers/CreateTagController" // Importando o controle do usuario 
-import { ensureAdmin } from "./middlewares/ensureAdmin"
-import { AuthenticateUserController } from "./controllers/AuthenticateUserController"
-import { CreateComplimentController } from "./controllers/CreateComplimentController"
+import { Router } from "express";
+import { CreateUserController } from "./controllers/CreateUserController";
+import { CreateTagController } from "./controllers/CreateTagController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateComplimentController } from "./controllers/CreateComplimentController";
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
+import { ListUserSendComplimentsController } from "./controllers/ListUserSendComplimentsController";
+import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController";
+import { ListTagsController } from "./controllers/ListTagsController";
+import { ListUsersController } from "./controllers/ListUsersController";
 
-const router = Router() // Rota
+const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
-const authenticateUserController = new AuthenticateUserController()
-const createComplimentController = new CreateComplimentController()
+const authenticateUserController = new AuthenticateUserController();
+const createComplimentController = new CreateComplimentController();
+const listUserSendComplimentsController =
+  new ListUserSendComplimentsController();
+const listUserReceiveComplimentsController =
+  new ListUserReceiveComplimentsController();
 
-router.post("/users", createUserController.handle) // Criando uma rota post para criar usuarios 
-router.post("/login", authenticateUserController.handle)
-router.post("/tags", ensureAdmin, createTagController.handle) // Criando uma rota post para criar uma tag 
-router.post("/compliment", createComplimentController.handle )
+const listTagsController = new ListTagsController();
 
-export { router } // Exportando rota
+const listUsersController = new ListUsersController();
+
+router.post(
+  "/tags",
+  ensureAuthenticated,
+  ensureAdmin,
+  createTagController.handle
+);
+
+router.get("/tags", ensureAuthenticated, listTagsController.handle);
+
+router.post("/users", createUserController.handle);
+router.post("/login", authenticateUserController.handle);
+router.post(
+  "/compliments",
+  ensureAuthenticated,
+  createComplimentController.handle
+);
+
+router.get(
+  "/users/compliments/send",
+  ensureAuthenticated,
+  listUserSendComplimentsController.handle
+);
+router.get(
+  "/users/compliments/receive",
+  ensureAuthenticated,
+  listUserReceiveComplimentsController.handle
+);
+
+router.get("/users", ensureAuthenticated, listUsersController.handle);
+
+export { router };
